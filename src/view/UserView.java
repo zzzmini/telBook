bc5421005c97b3dd41be35bbaccb71ad07302dd8
zzzmini <zzzmini@gmail.com>
2026-04-21 09:44:03 +0900

    feat : Main과 UserView 연결 작업 완료

---
+++ src/view/UserView.java
@@ -0,0 +1,26 @@
+package view;
+
+import java.util.Scanner;
+
+public class UserView {
+    private final Scanner scanner;
+
+    public UserView(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public void insert() {
+    }
+
+    public void update() {
+    }
+
+    public void delete() {
+    }
+
+    public void searchAll() {
+    }
+
+    public void searchOne() {
+    }
+}
