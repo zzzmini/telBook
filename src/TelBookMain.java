33f371d3b7ba243712addd62a07be447c83060a2
zzzmini <zzzmini@gmail.com>
2026-04-22 12:08:35 +0900

    feat : 종료처리 완

--- src/TelBookMain.java
+++ src/TelBookMain.java
@@ -46,6 +46,7 @@ public class TelBookMain {
                     userView.searchOne();
                     break;
                 case 6:
+                    DBConn.close();
                     System.out.println("종료합니다.");
             }
         }
